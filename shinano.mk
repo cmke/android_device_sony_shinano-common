0ab623e9398c97f3a3fdb13166210ce3c3f975dd
Petr kecinzer Řezníček <kecinzer@gmail.com>
2015-02-01 17:02:54 +0100

    Use 3GB of HWUI memory
    Change-Id: I59d771c34e0f766ff4d8401609c2b1b0c5c0647d
    Signed-off-by: Petr kecinzer Řezníček <kecinzer@gmail.com>

--- shinano.mk
+++ shinano.mk
@@ -211,7 +211,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 $(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/config/config-bcm.mk)
 
 # HWUI memory limits
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
+$(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-hwui-memory.mk)
 
 # Include non-opensource parts
 $(call inherit-product, vendor/sony/shinano-common/shinano-common-vendor.mk)
